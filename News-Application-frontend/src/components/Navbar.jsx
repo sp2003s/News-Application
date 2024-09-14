e45d9a48d3f3bfa4d73459802a88b731c09a005e
Shaurya Pratap Singh <shauryajt3455@gmail.com>
2024-09-14 17:28:08 +0530

    Project initialization and Navbar

---
+++ News-Application-frontend/src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Navbar = () => {
+
+    const today = new Date().toLocaleDateString()
+
+  return (
+    <div className='flex flex-col pt-2 shadow-lg'>
+
+      <div className='flex flex-auto justify-between'>
+        <p className='pt-2 font-serif'>Date: {today}</p>
+        <h1 className='text-6xl p-2 text-center font-bold font-customFont'>Daily Dispatch</h1>
+
+        <div className='pt-1'>
+          <button className='mx-1 p-1 w-[90px] border-2 border-red-600 rounded text-white bg-red-600'>Subscribe</button>
+          <button className='mx-1 p-1 w-[90px] border-2 border-red-600 rounded text-red-600 hover:bg-red-600 hover:text-white hover:transition-all'>Login</button>
+        </div>
+
+      </div>
+        <br />
+
+        <div className='flex flex-row mt-5 justify-center mx-10'>
+            <div className='font-serif w-[230px] text-xl mx-8 pr-10 cursor-pointer font-bold border-t-4 border-black'>India</div>
+            <div className='font-serif w-[230px] text-xl mx-8 pr-10 cursor-pointer font-bold border-t-4 border-black'>World</div>
+            <div className='font-serif w-[230px] text-xl mx-8 pr-10 cursor-pointer font-bold border-t-4 border-black'>Politics</div>
+            <div className='font-serif w-[230px] text-xl mx-8 pr-10 cursor-pointer font-bold border-t-4 border-black'>Technology</div>
+            <div className='font-serif w-[230px] text-xl mx-8 pr-10 cursor-pointer font-bold border-t-4 border-black'>E-Paper</div>
+        </div>
+        <br />
+    </div>
+  )
+}
+
+export default Navbar
