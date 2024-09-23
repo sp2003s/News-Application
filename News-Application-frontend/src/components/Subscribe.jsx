7c40cc56d1df2f368ebcd4f9d6de273e36825eac
Shaurya Pratap Singh <shauryajt3455@gmail.com>
2024-09-23 13:51:47 +0530

    adding heading, quotes panel and choose plan heading into subsciption page

--- News-Application-frontend/src/components/Subscribe.jsx
+++ News-Application-frontend/src/components/Subscribe.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const Subscribe = () => {
   return (
     <div>
-      
-    </div>
-  )
-}
 
-export default Subscribe
+      <h1 className="text-6xl p-2 text-center font-bold font-customFont">
+        Daily Dispatch
+      </h1>
+
+      <br /><br />
+
+      <div className="text-center w-full bg-white shadow-lg p-3 font-customFont text-xl">
+        "It's not just any Subsription", it's your pass to stay ahead of social media news
+        <br />
+        The most trusted news of the nation  
+      </div>
+
+      <br /><br /> <br />
+
+      <h1 className="text-center font-serif font-bold text-3xl">Choose your Plan: </h1>
+
+    </div>
+  );
+};
+
+export default Subscribe;
