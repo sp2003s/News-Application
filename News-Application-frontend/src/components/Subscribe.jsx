fd7077c9f8206f4c9b95070f06772f8d860f47da
Shaurya Pratap Singh <shauryajt3455@gmail.com>
2024-09-23 17:01:33 +0530

    div for subscription plan css

--- News-Application-frontend/src/components/Subscribe.jsx
+++ News-Application-frontend/src/components/Subscribe.jsx
@@ -18,7 +18,22 @@ const Subscribe = () => {
 
       <br /><br /> <br />
 
-      <h1 className="text-center font-serif font-bold text-3xl">Choose your Plan: </h1>
+      <h1 className="text-center font-serif font-bold text-3xl">Choose your Plan: </h1> <br /><br /><br />
+
+      <div className="flex justify-center items-center">
+        <div className="grid grid-cols-[1fr_1fr] gap-12 text-center">
+
+          <div className="w-[20vw] rounded-md p-3 bg-orange-100 cursor-pointer">
+            <p className="text-xl font-customFont">Daily+</p>
+          </div>
+          
+          <div className="w-[20vw] rounded-md p-3 bg-orange-200 cursor-pointer">
+            <p className="text-xl font-customFont">All Access+</p>
+            <p className="text-sm font-extralight">Recommended</p>
+          </div>
+
+        </div>
+      </div>
 
     </div>
   );
